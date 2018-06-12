20ef8432333a0f752b54d166deaeb0edc386fb0a
Denys Odaruschenko <deod@ciklum.com>
2018-06-12 19:57:03 +0300

    feature: material ui

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
 
 class Main extends Component {
   constructor() {
@@ -15,15 +18,18 @@ class Main extends Component {
       .then((data) => {
         const item = data.data.map(item => {
           return (
-            <div key={item._id.$id}>
+            <Grid key={item._id.$id} item xs={3}>
+              <div>{(item.advertisementPrice.sellPrice) ? 'zu verkaufen' : 'mieten'}</div>
               <img src={
                 (item.advertisementAssets['0']) ? item.advertisementAssets['0'].advertisementThumbnails.inventory_m.url : item.advertisementAssets.advertisementThumbnails.inventory_m.url
               }/>
               <div>{item.title}</div>
               <div>
-                <span>{item.realestateSummary.address.city}</span>/<span>{item.realestateSummary.address.street}</span>
+                <span>{item.realestateSummary.address.postalCode}</span>
+                <span>{item.realestateSummary.address.city}</span>
+                <span>/</span>
+                <span>{item.realestateSummary.address.street}</span>
               </div>
-
               <div>
                 <span>{(item.advertisementPrice.sellPrice) ? item.advertisementPrice.sellPrice : item.advertisementPrice.baseRent}</span>
                 <span>€</span>
@@ -36,29 +42,29 @@ class Main extends Component {
                 <span>{item.realestateSummary.space.toFixed(0)}</span>
                 <span>m<sup>2</sup></span>
               </div>
-            </div>
+              <Button variant="contained" color="primary">
+                Hello World
+              </Button>
+            </Grid>
           )
         });
 
         this.setState({
           item: item.slice(0, 10),
         });
-
       });
   }
 
   render() {
     return (
-      <div style={{width: 900}}>
+      <Grid container className={'container'} spacing={40}>
         {this.state.item}
-      </div>
+      </Grid>
     );
   }
 }
 
+ReactDOM.render(<Main/>, document.getElementById('root'));
 
-if (document.getElementById('root')) {
-  ReactDOM.render(<Main/>, document.getElementById('root'));
-}
 
 export default Main;
