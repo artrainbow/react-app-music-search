9a7ab21c591b16081ae2c86c1a4ba3f5e9e7e1c4
artrainbow <artrainbow@gmail.com>
2018-06-15 00:08:07 +0300

    feature: material ui

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,14 +1,18 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 import './main.scss';
 
 const classes = {
   container: 'container',
   containerItem: 'container-item',
   containerItemWrapper: 'container-item-wrapper',
+  media: 'media',
   card: 'card',
 };
 
@@ -29,33 +33,95 @@ class Main extends Component {
           return (
             <Grid key={item._id.$id} item xs={4} className={classes.containerItem}>
               <div className={classes.containerItemWrapper}>
-                <div className="card">
-                  <div>{(item.advertisementPrice.sellPrice) ? 'zu verkaufen' : 'mieten'}</div>
-                  <img src={
-                    (item.advertisementAssets['0']) ? item.advertisementAssets['0'].advertisementThumbnails.inventory_m.url : item.advertisementAssets.advertisementThumbnails.inventory_m.url
-                  }/>
-                  <div>{item.title}</div>
-                  <div>
-                    <span>{item.realestateSummary.address.postalCode}</span>
-                    <span>{item.realestateSummary.address.city}</span>
-                    <span>/</span>
-                    <span>{item.realestateSummary.address.street}</span>
-                  </div>
-                  <div>
-                    <span>{(item.advertisementPrice.sellPrice) ? item.advertisementPrice.sellPrice : item.advertisementPrice.baseRent}</span>
-                    <span>€</span>
-                  </div>
-                  <div>
-                    <span>{item.realestateSummary.numberOfRooms}</span>
-                    <span>Zimmer</span>
-                    <span>|</span>
-                    <span>ab</span>
-                    <span>{item.realestateSummary.space.toFixed(0)}</span>
-                    <span>m<sup>2</sup></span>
-                  </div>
-                  <Button variant="contained" color="primary">
-                    Hello World
-                  </Button>
+                <div>
+                  <Paper
+                    elevation={4}
+                    style={{
+                      position: 'absolute',
+                      marginTop: 25,
+                      marginLeft: 25,
+                    }}>
+                    <Typography variant={'title'}
+                                style={{
+                                  padding: '8px 10px 8px 10px',
+                                  fontWeight: 600,
+                                  fontSize: '100%',
+                                  color: 'rgba(0, 0, 0, 0.3)'
+                                }}>
+                      {item.advertisementPrice.sellPrice ? 'Kaufen' : 'Mieten'}
+                    </Typography>
+                  </Paper>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.media}
+                      image={
+                        item.advertisementAssets['0'] ? item.advertisementAssets['0'].advertisementThumbnails.inventory_m.url : item.advertisementAssets.advertisementThumbnails.inventory_m.url
+                      }
+                      title={item.title}
+                    />
+                    <CardContent
+                      style={{
+                        padding: '45px 50px 20px 50px',
+                      }}>
+                      <Typography gutterBottom variant="headline" component="h2"
+                                  style={{
+                                    fontWeight: 600,
+                                    fontSize: 20,
+                                    height: 52,
+                                    color: 'rgba(0, 0, 0, 0.6)',
+                                    overflow: 'hidden',
+                                  }}>
+                        {item.title}
+                      </Typography>
+                      <Typography component="span"
+                                  style={{
+                                    paddingTop: 10,
+                                    color: 'rgba(0, 0, 0, 0.3)',
+                                    fontWeight: 800,
+                                    fontSize: 16,
+                                    height: 24,
+                                    overflow: 'hidden',
+                                  }}>
+                        {item.realestateSummary.address.postalCode}
+                        <span> / </span>
+                        {item.realestateSummary.address.city}
+                        {item.realestateSummary.address.street}
+                      </Typography>
+                      <Typography component="span"
+                                  style={{
+                                    marginTop: 56,
+                                    color: 'rgba(0, 0, 0, 0.6)',
+                                    fontWeight: 700,
+                                    fontSize: 22,
+                                    float: 'left',
+                                  }}>
+                        {(item.advertisementPrice.sellPrice) ? item.advertisementPrice.sellPrice : item.advertisementPrice.baseRent}
+                        <span> €</span>
+                      </Typography>
+                      <Typography component="span"
+                                  style={{
+                                    marginTop: 52,
+                                    color: 'rgba(0, 0, 0, 0.6)',
+                                    fontSize: 19,
+                                    float: 'right',
+                                  }}>
+                        {item.realestateSummary.numberOfRooms}
+                        <span> Zimmer</span>
+                        <span
+                          style={{
+                            display: 'inline-block',
+                            height: 40,
+                            marginRight: 20,
+                            marginLeft: 32,
+                            verticalAlign: 'top',
+                            borderRight: '1px solid rgba(0, 0, 0, 0.6)'
+                          }}/>
+                        <span>ab </span>
+                        {item.realestateSummary.space.toFixed(0)}
+                        <span> m<sup style={{fontSize: '60%', fontWeight: 600}}>2</sup></span>
+                      </Typography>
+                    </CardContent>
+                  </Card>
                 </div>
               </div>
             </Grid>
