4713d55462372f1f9b095ce2d0a01061d1c6bea0
Denys Odaruschenko <deod@ciklum.com>
2019-09-03 11:50:07 +0300

    style:  prettifying server.js

--- server.js
+++ server.js
@@ -4,28 +4,24 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
 const config = require('./webpack.dev.js');
-
 const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
+    publicPath: config.output.publicPath,
 }));
 
-
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,DELETE,POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,DELETE,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
 // Serve the files on port 3000.
-app.get('c', function(req, res){
-  //console.log(req, res);
-  res.send('hello world');
+app.get('/files/', (req, res) => {
+    res.send('files list');
 });
-
 app.listen(3000);
