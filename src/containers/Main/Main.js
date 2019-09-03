9f65377dd48126054d407378203acab2201f69de
Denys Odaruschenko <deod@ciklum.com>
2019-09-03 11:50:36 +0300

    feat: new features

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -2,153 +2,201 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
 import './main.scss';
 
 const classes = {
-  container: 'container',
-  containerItem: 'container-item',
-  containerItemWrapper: 'container-item-wrapper',
-  media: 'media',
-  card: 'card',
+    container: 'container',
+    containerItem: 'container-item',
+    containerItemWrapper: 'container-item-wrapper',
+    media: 'media',
+    card: 'card',
 };
 
-
 class Main extends Component {
-  constructor() {
-    super();
-    this.state = {
-      item: [],
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            isFetched: false,
+            isPlaying: true,
+            name: 'type a singer',
+        };
+    }
 
-  componentDidMount() {
-    fetch('https://cors.io/?https://api.mcmakler.de/v1/advertisements')
-      .then(res => res.json())
-      .then((data) => {
-        const item = data.data.map(item => {
-          return (
-            <Grid key={item._id.$id} item xs={4} className={classes.containerItem}>
-              <div className={classes.containerItemWrapper}>
-                <div>
-                  <Paper
-                    elevation={4}
-                    style={{
-                      position: 'absolute',
-                      marginTop: 25,
-                      marginLeft: 25,
-                    }}>
-                    <Typography variant={'title'}
-                                style={{
-                                  padding: '8px 10px 8px 10px',
-                                  fontWeight: 600,
-                                  fontSize: '100%',
-                                  fontFamily: 'MSlondonRegular',
-                                  color: 'rgba(0, 0, 0, 0.3)'
-                                }}>
-                      {item.advertisementPrice.sellPrice ? 'Kaufen' : 'Mieten'}
-                    </Typography>
-                  </Paper>
-                  <Card className={classes.card}>
-                    <CardMedia
-                      className={classes.media}
-                      image={
-                        item.advertisementAssets['0'] ? item.advertisementAssets['0'].advertisementThumbnails.inventory_m.url : item.advertisementAssets.advertisementThumbnails.inventory_m.url
-                      }
-                      title={item.title}
-                    />
-                    <CardContent style={{padding: '45px 50px 20px 50px'}}>
-                      <Typography gutterBottom variant="headline" component="h2"
-                                  style={{
-                                    fontWeight: 600,
-                                    fontSize: 20,
-                                    fontFamily: 'MSlondonRegular',
-                                    height: 52,
-                                    color: 'rgba(0, 0, 0, 0.6)',
-                                    overflow: 'hidden',
-                                  }}>
-                        {item.title}
-                      </Typography>
-                      <Typography component="span"
-                                  style={{
-                                    paddingTop: 10,
-                                    color: 'rgba(0, 0, 0, 0.3)',
-                                    fontWeight: 800,
-                                    fontFamily: 'MSlondonRegular',
-                                    fontSize: 16,
-                                    height: 24,
-                                    overflow: 'hidden',
-                                  }}>
-                        {item.realestateSummary.address.postalCode}
-                        <span> / </span>
-                        {item.realestateSummary.address.city}
-                        {item.realestateSummary.address.street}
-                      </Typography>
-                      <Typography component="span"
-                                  style={{
-                                    marginTop: 56,
-                                    color: 'rgba(0, 0, 0, 0.6)',
-                                    fontWeight: 700,
-                                    fontFamily: 'MSlondonRegular',
-                                    fontSize: 22,
-                                    float: 'left',
-                                  }}>
-                        {(item.advertisementPrice.sellPrice) ? item.advertisementPrice.sellPrice : item.advertisementPrice.baseRent}
-                        <span> €</span>
-                      </Typography>
-                      <Typography component="span"
-                                  style={{
-                                    marginTop: 52,
-                                    color: 'rgba(0, 0, 0, 0.6)',
-                                    fontSize: 19,
-                                    fontFamily: 'MSlondonRegular',
-                                    float: 'right',
-                                  }}>
-                        {item.realestateSummary.numberOfRooms}
-                        <span> Zimmer</span>
-                        <span
-                          style={{
-                            display: 'inline-block',
-                            height: 40,
-                            marginRight: 20,
-                            marginLeft: 32,
-                            verticalAlign: 'top',
-                            borderRight: '1px solid rgba(0, 0, 0, 0.6)'
-                          }}/>
-                        <span>ab </span>
-                        {item.realestateSummary.space.toFixed(0)}
-                        <span> m<sup
-                          style={{
-                            fontSize: '60%',
-                            fontWeight: 600
-                          }}>
-                          2</sup>
-                        </span>
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </div>
-              </div>
+    componentDidMount() {
+        const {name} = this.state;
+
+        fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${name}`, {
+            'method': 'GET',
+            'headers': {
+                'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
+                'x-rapidapi-key': 'fd9b15ae79mshccc19dbccbef94ep17f3e6jsn377328e6e83e',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    items: data,
+                    isFetched: true,
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    render() {
+        const {
+            name,
+            items,
+        } = this.state;
+
+        const handleChange = (event) => {
+            this.setState({name: event.target.value});
+
+            setTimeout(() => {
+                if (name.length > 2) this.componentDidMount();
+            }, 0);
+        };
+
+        return (
+            <Grid container className={classes.container} spacing={0}>
+                <Grid container
+                      style={{
+                          marginTop: 10,
+                      }}>
+                    <FormControl
+                        style={{
+                            marginLeft: 30,
+                        }}
+                        className={classes.formControl}>
+                        <InputLabel htmlFor="name-simple">Search</InputLabel>
+                        <Input id="name-simple" value={name} onChange={handleChange}/>
+                    </FormControl>
+                </Grid>
+
+                <Items data={items}/>
             </Grid>
-          );
-        });
+        );
+    }
+}
 
-        this.setState({
-          item: item.slice(0, 10),
-        });
-      });
-  }
+class Items extends Main {
+    constructor(props) {
+        super(props);
 
-  render() {
-    return (
-      <Grid container className={classes.container} spacing={0}>
-        {this.state.item}
-      </Grid>
-    );
-  }
+        this.player = element => {
+            this.playerData = element || '';
+        };
+    }
+
+    static preLoader(id) {
+        document.getElementById(id).removeAttribute('class');
+    }
+
+    render() {
+        const {
+            isFetched,
+            isPlaying,
+        } = this.state;
+
+        if (isFetched && this.props.data) {
+            const play = (url) => {
+                if (this.playerData.src === url) {
+                    if (isPlaying) {
+                        this.playerData.pause();
+                        this.setState({isPlaying: false});
+                    } else {
+                        this.playerData.play();
+                        this.setState({isPlaying: true});
+                    }
+                } else {
+                    this.playerData.src = url;
+                    this.playerData.play();
+                    this.setState({isPlaying: true});
+                }
+            };
+
+            return this.props.data.data.map(item => {
+                return (
+                    <Grid key={item.id} item xs={3} className={classes.containerItem}>
+                        <div className={classes.containerItemWrapper}>
+                            <div>
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                        style={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                        }}
+                                        className={classes.media}
+                                        image={item.album.cover_big}
+                                        title={item.title}>
+                                        <SvgIcon
+                                            onClick={() => {
+                                                play(item.preview);
+                                            }}
+                                        >
+                                            <path
+                                                fill="#FFFFFF"
+                                                d="M2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12M10,17L15,12L10,7V17Z"/>
+                                        </SvgIcon>
+                                        <audio ref={this.player} onCanPlay={() => {
+                                            Items.preLoader(item.id);
+                                        }}>
+                                            <source src={item.preview}/>
+                                        </audio>
+                                        <div id={item.id} className={'spinner'}/>
+                                    </CardMedia>
+                                    <CardContent style={{padding: '30'}}>
+                                        <Typography gutterBottom variant="headline" component="h1"
+                                                    style={{
+                                                        fontWeight: 600,
+                                                        fontSize: 20,
+                                                        fontFamily: 'MSlondonRegular',
+                                                        height: 52,
+                                                        color: 'rgba(0, 0, 0, 0.6)',
+                                                        overflow: 'hidden',
+                                                    }}>
+                                            {item.title}
+                                        </Typography>
+                                        <Typography component="h2"
+                                                    style={{
+                                                        marginTop: 5,
+                                                        color: 'rgba(0, 0, 0, 0.3)',
+                                                        fontWeight: 800,
+                                                        fontFamily: 'MSlondonRegular',
+                                                        fontSize: 15,
+                                                        height: 24,
+                                                        overflow: 'hidden',
+                                                    }}>
+                                            {item.album.title}
+                                        </Typography>
+                                        <Typography component="span"
+                                                    style={{
+                                                        marginTop: 5,
+                                                        color: 'rgba(0, 0, 0, 0.6)',
+                                                    }}>
+                                            {item.artist.name}
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </div>
+                    </Grid>
+                );
+            });
+        } else {
+            return null;
+        }
+    }
 }
 
 ReactDOM.render(<Main/>, document.getElementById('root'));
